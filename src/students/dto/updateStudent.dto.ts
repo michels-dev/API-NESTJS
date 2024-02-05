5c30bdd0f6e62b603d034e9f2cc88ef0cfe99be7
michelsdev <sinambelamichel05@gmail.com>
2024-02-05 12:01:06 +0700

    packages dto

---
+++ src/students/dto/updateStudent.dto.ts
@@ -0,0 +1,27 @@
+export class UpdateStudentDto{
+    id: number;
+    date_time: Date;
+    modified: Date;
+    ip: string;
+    creator: number;
+    modifier: number;
+    nospj: string;
+    spj: string;
+    kodesek: string;
+    kelas: string;
+    noinduk: string;
+    no_urut_kelas: string;
+    no_urut_buku_induk: string;
+    nik_guru_term_1: string;
+    nik_guru_term_2: string;
+    nik_guru_term_3: string;
+    nik_guru_term_4: string;
+    namasiswa: string;
+    aktif: string;
+    foto: string;
+    keterangan: string;
+    status_aktif_sekolah: number;
+    notes: string;
+}
+
+export default UpdateStudentDto
