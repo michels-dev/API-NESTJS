b3fed139f2338284ae9432c024dbe508d16860f0
michelsdev <sinambelamichel05@gmail.com>
2024-02-05 13:59:16 +0700

    student controller

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { StudentsService } from './students.service';
+import CreateStudentDto from './dto/createStudent.dto';
+import UpdateStudentDto from './dto/updateStudent.dto';
 
 @Controller('students')
-export class StudentsController {}
+export class StudentsController {
+    constructor(private readonly studentsService: StudentsService) {}
+
+    // get all students
+    @Get()
+    getStudents(){
+        return this.studentsService.getAllStudents();
+    }
+
+    // get data student by id
+    @Get(':id')
+    getStudentById(@Param('id') id: string){
+        return this.studentsService.getStudentById(Number(id));
+    }
+
+    // create student
+    @Post()
+    async createStudent(@Body() student: CreateStudentDto){
+        return this.studentsService.createStudent(student);
+    }
+
+    // update student
+    @Put(':id')
+    async updateStudent(@Param('id') id:string, @Body() student: UpdateStudentDto){
+        return this.studentsService.updateStudent(Number(id), student);
+    }
+
+    // delete student
+    @Delete(':id')
+    async deleteStudent(@Param('id') id: string){
+        this.studentsService.deleteStudent(Number(id));
+    }
+}
